4c35b47b44f8e186b2784868a44834516e36aaa7
Srijan kumar <147295283+srijankumar8969@users.noreply.github.com>
2024-05-15 11:02:49 +0530

    objects.js

--- objects.js
+++ objects.js
@@ -1,3 +1,5 @@
+                                                                       Objects 
+
 // singleton  --> object lateral method se singleton object kabhi nahi banta hai
 // Object.create
 
@@ -36,3 +38,97 @@ JsUser.greetingTwo = function(){
 
 console.log(JsUser.greeting());
 console.log(JsUser.greetingTwo());
+
+
+// const tinderUser = new Object()          // is method se singleton object declare hota hai
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "hitesh",
+            lastname: "choudhary"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
+
+
+                                                           Adding two objects
+// const obj3 = { obj1, obj2 }  object 3 will take obj1 and object 2 as input 
+// const obj3 = Object.assign( obj1, obj2, obj4)   //is case me obj1 bhi change ho kar obj3 ke hi barabar ho chuka 
+// const obj3 = Object.assign({}, obj1, obj2, obj4)  //this wll remove the curly brace from the objects and will add them in first object thus first array also changes thus is important to transfer an empty array
+
+const obj3 = {...obj1, ...obj2} //spread operator
+// console.log(obj3);
+
+
+const users = [          //array of objects
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+]
+
+users[1].email
+// console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser));  //return keys as array
+// console.log(Object.values(tinderUser));  //return values as array
+// console.log(Object.entries(tinderUser));  return array in which each key value pair is a sub array 
+
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));  /to check whether a particular key is present in the the object
+
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+
+                                                          Object Destructuring
+// course.courseInstructor  //ye likhn me kapho bada hai is liye hum destructure karte hain objects ko
+
+//const {courseInstructor} = course  // ye karne se courseInstructor ko hi use karke hum directrly iski value ko access kar sakte hain 
+const {courseInstructor: instructor} = course //is tarike se destructure karke hum koi aur naam bhi assign kar sakte hain
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+                                                        API and JSON
+
+
+
+// {
+//     "name": "hitesh",
+//     "coursename": "js in hindi",
+//     "price": "free",
+//       "check":true
+// }
+
+[
+    {},
+    {},
+    {}
+]
