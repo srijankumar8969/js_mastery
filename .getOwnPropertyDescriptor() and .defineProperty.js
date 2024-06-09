9eefc784e0f575120da170dcd6434eb96f487af6
Srijan kumar <147295283+srijankumar8969@users.noreply.github.com>
2024-06-09 12:46:04 +0530

    Create .getOwnPropertyDescriptor() and .defineProperty.js

---
+++ .getOwnPropertyDescriptor() and .defineProperty.js
@@ -0,0 +1,33 @@
+const descripter = Object.getOwnPropertyDescriptor(Math, "PI")  // this will show all the description related to the property of the object
+// console.log(descripter);
+
+// console.log(Math.PI);
+// Math.PI = 5
+// console.log(Math.PI);
+
+const chai = {
+    name: 'ginger chai',
+    price: 250,
+    isAvailable: true,
+
+    orderChai: function(){
+        console.log("chai nhi bni");
+    }
+}
+
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+Object.defineProperty(chai, 'name', {
+    //writable: false,
+    enumerable: true,
+    
+})
+
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+for (let [key, value] of Object.entries(chai)) {
+    if (typeof value !== 'function') {
+        console.log(`${key} : ${value}`);
+    }
+}
+//to understand completely about looping through objects use https://www.freecodecamp.org/news/how-to-iterate-over-objects-in-javascript/
