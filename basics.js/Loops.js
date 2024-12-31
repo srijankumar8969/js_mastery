284b8a27b88fd29a925658d2034a063bee38ce94
Srijan kumar <147295283+srijankumar8969@users.noreply.github.com>
2024-12-31 12:00:34 +0530

    Update Loops.js

--- basics.js/Loops.js
+++ basics.js/Loops.js
@@ -1,4 +1,4 @@
-// for
+// for while do {} while
 
 for (let i = 0; i <= 10; i++) {
     const element = i;
@@ -24,7 +24,6 @@ for (let index = 0; index < myArray.length; index++) {
     const element = myArray[index];
     //console.log(element);    
 }
-
 
 // break and continue
 
